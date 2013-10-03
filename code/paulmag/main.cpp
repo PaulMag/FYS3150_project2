6b3f9ed0185985c4f8b20dbac4a42ded338222bc
PaulMag <paulms@ulrik.uio.no>
2013-10-03 17:36:18 +0200

    Fixed max value finder so it excludes the diagonal. Fixed so that the matrix A is updated symmetricaly for each iteration. Something is wrong, as the resulting matrix contains lots of nan.

--- code/paulmag/main.cpp
+++ code/paulmag/main.cpp
@@ -32,17 +32,32 @@ int main() {
 
     cout << A << endl;
 
-    uword k, l;
+    int k = 0;
+    int l = 0;
+
     double a_ll, a_lk, a_kl, a_kk;
     double tau, t, s, c;
     double b_ik, b_il;
 
     double eps = 1e-8;
     double maxA = 10 * eps;
+    int iterations = 0;
 
     while (maxA > eps) {
 
-        maxA = A.max(k, l); // TODO: doesn't exclude diagonal!!!
+        maxA = 0;
+        for (int i=0; i<n-1; i++) {
+            if ( maxA < abs(A(i+1,i)) ) {
+                maxA = A(i+1,i);
+                k = i+1;
+                l = i;
+            }
+            if ( maxA < abs(A(i,i+1)) ) {
+                maxA = A(i,i+1);
+                k = i;
+                l = i+1;
+            }
+        }
 
         a_kk = A(k,k);
         a_kl = A(k,l);
@@ -59,12 +74,18 @@ int main() {
             b_ik = A(i,k) * c - A(i,l) * s;
             b_il = A(i,l) * c - A(i,k) * s;
             A(i,k) = b_ik;
+            A(k,i) = b_ik;
             A(i,l) = b_il;
+            A(l,i) = b_il;
         }
         A(k,k) = a_kk * c*c - 2*a_kl * c * s + a_ll * s*s;
         A(l,l) = a_ll * c*c - 2*a_kl * c * s + a_kk * s*s;
         A(k,l) = 0;
+        A(l,k) = 0;
+
+        iterations++;
     }
 
     cout << A << endl;
+    cout << "Iterations: " << iterations << endl;
 }

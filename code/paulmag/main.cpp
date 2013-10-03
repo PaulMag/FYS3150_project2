d11963772ea87d337283be610633fff6ced48622
PaulMag <paulms@ulrik.uio.no>
2013-10-03 13:58:37 +0200

    Created main c++ file.

---
+++ code/paulmag/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <armadillo>
+#include "time.h"
+
+using namespace std;
+using namespace arma;
+
+/*
+ *  I am beautiful.
+ *  Vedad is not beautiful, because he laughed at me.
+ */
+
+int main() {
+
+int A = 1;
+
+
+}

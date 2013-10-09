8ca2b45c01a09e76b3bcd0ccc65004f194cfdcb0
PaulMag <paulms@ulrik.uio.no>
2013-10-09 02:41:16 +0200

    Made code more orderly by taking the Jacobi algorithm out of main.cpp and placing it into a separate file. Also cleaned away old comments.

--- code/paulmag/main.cpp
+++ code/paulmag/main.cpp
@@ -2,7 +2,9 @@
 #include <cmath>
 #include <fstream>
 #include <armadillo>
+
 #include "time.h"
+#include "JacobiRotation.cpp"
 
 using namespace std;
 using namespace arma;
@@ -12,7 +14,8 @@ using namespace arma;
  *  Vedad is not beautiful, because he laughed at me.
  */
 
-/* Stuff to remember:
+/*
+ * Stuff to remember:
  *
  * mat A = zeros<mat>(n, n);
  * vec a(n);
@@ -20,8 +23,7 @@ using namespace arma;
 
 int main() {
 
-    int n = 168;
-    //int nStep = n + 1;
+    int n = 100; // nStep = n+1
 
     double rhoMin = 0;
     double rhoMax = 4.5; // 4.5 is ideal according to vedadh
@@ -41,83 +43,19 @@ int main() {
     rho = rhoMin + n * h;
     V = rho*rho;
     A(n-1,n-1) = 2 / (h*h) + V;
-    //A(n-1,n-1) = 2 / (h*h) + pow(rhoMin + n*h, 2);
 
-    //cout << A << endl;
-
-    int k, l;
-
-    double a_ll, a_lk, a_kl, a_kk;
-    double tau, t, s, c;
-    double b_ik, b_il;
-
-    double eps = 1e-8;
-    double maxA = 10 * eps;
-    int iterations = 0;
-
-    while (maxA > eps) {
-
-        maxA = 0;
-        for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                if (abs(A(i,j)) > maxA ) {
-                    // Find max value except on diagonal.
-                    maxA = abs(A(i,j));
-                    k = i;
-                    l = j;
-                }
-            }
-        }
-
-        a_kk = A(k,k);
-        a_kl = A(k,l);
-        a_lk = A(l,k);
-        a_ll = A(l,l);
-
-        tau = (a_ll - a_kk) / (2 * a_kl);
-        if (tau > 0) {
-            t = 1. / (tau + sqrt(1 + tau*tau));
-        }
-        else {
-            t = 1. / (tau - sqrt(1 + tau*tau));
-        }
-
-        c = 1.0 / sqrt(1 + t*t); // cos(theta)
-        s = t * c;             // sin(theta)
-
-        for (int i=0; i<n; i++) {
-            if (i!=k and i!=l) {
-                b_ik = A(i,k) * c - A(i,l) * s;
-                b_il = A(i,l) * c + A(i,k) * s;
-                A(i,k) = b_ik;
-                A(k,i) = b_ik;
-                A(i,l) = b_il;
-                A(l,i) = b_il;
-            }
-        }
-
-        A(k,k) = a_kk * c*c - 2*a_kl * c * s + a_ll * s*s;
-        A(l,l) = a_ll * c*c + 2*a_kl * c * s + a_kk * s*s;
-        A(k,l) = 0.0;
-        A(l,k) = 0.0;
-        //cout << (a_kk - a_ll) * c*s + a_kl * (c*c - s*s) << endl;
-
-        //cout << A << endl;
-        iterations++;
-        //cout << maxA << endl;
-        //break;
-    }
+    A = JacobiRotation(A, n);
 
     //vec eigenVals = sort(eig_sym(A)); // Armadillos method
 
-    cout << "Iterations: " << iterations << endl;
     cout << "Eigenvalues: " << endl;
     vec eigenVals = sort(A.diag());
     cout << eigenVals(0) << endl <<
             eigenVals(1) << endl <<
             eigenVals(2) << endl;
 
-    /* When nStep \approx 168 then 3 lowest eigenvalues are correct
+    /*
+     * When nStep \approx 168 then 3 lowest eigenvalues are correct
      * with 4 leading digits, rhoMax = 4.5.
      */
 
